d775de11f55dca2a04115426f3718265a5b34464
Felipe Decome <felipe.decome@hotmail.com>
2020-08-10 12:21:26 -0300

    setting themes and global styles

---
+++ web/src/assets/styles/colors.ts
@@ -0,0 +1,14 @@
+export default {
+  white: '#FBF9FF',
+  lighterGray: '#F8F8F8',
+  lightGray: '#ECECEC',
+  lightGrayDark: '#DFDFDF',
+  darkGrayLight: '#C2C2C2',
+  darkGray: '#7A7E82',
+  darkerGray: '#283239',
+  black: '#040F16',
+
+  primary: '#28BBDC',
+  secondary: '#6340FF',
+  tertiary: '#A512CA',
+};
---
+++ web/src/assets/styles/themes/light.ts
@@ -0,0 +1,17 @@
+import colors from '../colors';
+
+export default {
+  title: 'light',
+
+  colors: {
+    primary: colors.primary,
+    secondary: colors.secondary,
+    tertiary: colors.tertiary,
+
+    textInColors: colors.white,
+
+    background: colors.lighterGray,
+    headerBackground: colors.white,
+    footerBackground: colors.darkerGray,
+  },
+};
